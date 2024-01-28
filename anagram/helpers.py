23629210d1a8edca19abb15623408a5454b352e3
Eimantas Nejus <94107704+eimantas-nejus-cct@users.noreply.github.com>
2024-01-28 15:24:40 +0200

    ADR-5: Refactor code to improve readability

--- anagram/helpers.py
+++ anagram/helpers.py
@@ -4,3 +4,14 @@ from django.forms import NullBooleanField
 def to_python_bool(value: str | bool | None) -> bool:
     """Convert a value to a boolean value. Convenient to parse request query parameters."""
     return NullBooleanField().to_python(value)
+
+
+def calculate_median(lengths: list[int]) -> float | None:
+    if not lengths:
+        return None
+
+    count = len(lengths)
+    if count % 2 == 1:
+        return lengths[count // 2]
+    else:
+        return (lengths[count // 2 - 1] + lengths[count // 2]) / 2
